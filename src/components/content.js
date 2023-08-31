dd8d31df1d170982026cebd6447b38eccef22015
Satvik Ms <satvik.ms@intimetec.com>
2023-08-31 21:05:38 +0530

    pagination changes

--- src/components/content.js
+++ src/components/content.js
@@ -1,14 +1,17 @@
+import React, { useState, useEffect } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 import './content.css';
-import React, { useState, useEffect } from 'react';
 import { getCardsData } from '../services/usersService';
 
 function Content({ isOpen }) {
   const [data, setData] = useState([]);
   const [gridApi, setGridApi] = useState(null);
   const [searchText, setSearchText] = useState('');
+  const [paginationPageSize, setPaginationPageSize] = useState(10);
+  const [paginationCurrentPage, setPaginationCurrentPage] = useState(1);
+  const [paginationTotalPages, setPaginationTotalPages] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,6 +19,7 @@ function Content({ isOpen }) {
         const response = await getCardsData();
         if (Array.isArray(response.cards)) {
           setData(response.cards);
+          setPaginationTotalPages(Math.ceil(response.cards.length / paginationPageSize));
         }
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -23,15 +27,25 @@ function Content({ isOpen }) {
     };
 
     fetchData();
-  }, []);
+  }, [paginationPageSize]);
 
   const onGridReady = (params) => {
     setGridApi(params.api);
+
+    const firstPageData = data.slice(0, paginationPageSize);
+    params.api.setRowData(firstPageData);
   };
 
   const onSearchTextChange = (event) => {
     setSearchText(event.target.value);
     gridApi.setQuickFilter(event.target.value);
+  };
+
+  const onPageChange = (newPage) => {
+    setPaginationCurrentPage(newPage);
+    const startIndex = (newPage - 1) * paginationPageSize;
+    const pageData = data.slice(startIndex, startIndex + paginationPageSize);
+    gridApi.setRowData(pageData);
   };
 
   const columnDefs = [
@@ -42,11 +56,9 @@ function Content({ isOpen }) {
     { headerName: 'Card Expiration', field: 'cardExpiration', filter: true },
   ];
 
-  const rowData = data;
-
   return (
     <div className={`content ${isOpen ? 'shifted' : ''} ag-theme-alpine`} style={{ height: '400px', width: '100%' }}>
-      <h2>Records Count: <span>{rowData.length}</span></h2>
+      <h2>Records Count: <span>{data.length}</span></h2>
       <div className="search-container">
         <input
           type="text"
@@ -57,9 +69,18 @@ function Content({ isOpen }) {
       </div>
       <AgGridReact
         columnDefs={columnDefs}
-        rowData={rowData}
+        rowData={data.slice((paginationCurrentPage - 1) * paginationPageSize, paginationCurrentPage * paginationPageSize)}
         onGridReady={onGridReady}
       />
+      <div className="pagination-controls">
+        <button disabled={paginationCurrentPage === 1} onClick={() => onPageChange(paginationCurrentPage - 1)}>
+          Previous
+        </button>
+        <span>Page {paginationCurrentPage} of {paginationTotalPages}</span>
+        <button disabled={paginationCurrentPage === paginationTotalPages} onClick={() => onPageChange(paginationCurrentPage + 1)}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
